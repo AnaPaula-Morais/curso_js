5f6eddb6577c915d043207bb75334443859ad1c3
Ana Paula <ap.almeidapm@gmail.com>
2022-04-19 16:12:42 +0100

    como modifica o html e usar o refresh e redirect

---
+++ modificandoHtml/script.js
@@ -0,0 +1,14 @@
+function trcardiv() {
+    var area = document.getElementById("area");
+    var texto = prompt("Digite alguma coisa");
+
+    area.innerHTML = texto;
+
+}
+
+function clicar(nome) {
+    var area = document.getElementById("area");
+    var texto = prompt("Qual o seu sobrenome?");
+
+    area.innerHTML = nome + " " + texto;
+}
