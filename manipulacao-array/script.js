866fde282e3061c6fe4e3d1744f71d96b9d1f0d4
Ana Paula <ap.almeidapm@gmail.com>
2022-11-22 10:36:46 +0000

    exemplos de funcoes e arrays

--- manipulacao-array/script.js
+++ manipulacao-array/script.js
@@ -1,3 +1,4 @@
+
 var lista = ["caderno", "lapis", "borracha", "caneta"]
     //para buscar no array um valor
 lista.indexOf("lapis");
@@ -9,3 +10,8 @@ lista.pop();
 lista.shift();
 //como add um ítem novo no array
 lista.push("resma de papel")
+// addum itm no inicio da lista
+lista.unshift("apontador");
+// encontrar a posição de um elemento no array
+let index = lista.indexOf("lapis");
+console.log(index);
