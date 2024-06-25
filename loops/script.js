8960de107878444f01acc814d3824ce9815b6eb7
Ana Paula <ap.almeidapm@gmail.com>
2024-06-25 14:24:38 +0100

    exemplos com arrow function, loops, manipulacao de array e manipulacao do DOM

--- loops/script.js
+++ loops/script.js
@@ -1,5 +1,5 @@
 //usando o while
-var x = 0
+/*var x = 0
 document.write("Iniciando o loop...<br/><br/>")
 while (x < 10) {
     document.write("Número: " + x + "<br/>");
@@ -30,4 +30,44 @@ switch (x) {
     default:
         alert("Ninguem foi satisfeito");
         break;
+}*/
+
+/*let numero = 0;
+while(numero < 10){
+    console.log(`O número da vez é: ${numero}`)
+    numero++;
+}*/
+
+/*let fruits = ["maça","uva","banana"];
+for(let i in fruits){
+    console.log(fruits[i]);
 }
+
+var numero = 1;
+while(numero <= 100){
+    console.log(`Número ${numero}`);
+    numero++;
+}*/
+
+let fruits = ["maça", "uva", "laranja", "banana"];
+fruits.sort();
+fruits.reverse();
+console.log(fruits);
+
+let cars = [
+    {Brand: "Fiat", Year: 2022},
+    {Brand: "Ferrari", Year: 2020},
+    {Brand: "BMW", Year: 2018}
+]
+
+cars.sort((a,b) => {
+    if(a.Year > b.Year){
+        return 1;
+    }else if(a.Year < b.Year){
+        return -1;
+    }else{
+        return 0;
+    }
+})
+
+console.log(cars);
